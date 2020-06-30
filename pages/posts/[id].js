f44842039c0438befc78fe133ff07f72d7b611a9
Joy Bhalla <joybhalla9@gmail.com>
2020-06-30 18:00:31 +0530

    final commit before inital development

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,23 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Data from '../../components/date';
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date}/>
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
@@ -22,7 +31,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
